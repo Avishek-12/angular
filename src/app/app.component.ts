77343a559cac040cb03800924c46b443a1ee15ba
@ <@>
2022-09-29 18:55:24 +0530

    my First AngularJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Musicaffiene';
+
+  count = 0;
+
+  number1 = 1;
+  number2 = 2;
+  result = 0;
+
+
+  listofnumbers = [1,23,45,576,3];
+
+  listofnames = ["avi","arka","pri","ankur","muski"];
+
+  increment = () => {
+    this.count++;
+  }
+
+  
+  decrement = () => {
+    this.count--;
+  }
+
+  Add = () => {
+    this.result=this.number1+this.number2;
+  }
 }
